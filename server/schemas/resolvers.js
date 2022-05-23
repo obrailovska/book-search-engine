6a0bd45c1cc5c183ed4d3182d64e815fc7fecdef
obrailovska <olabrrai@icloud.com>
2022-05-22 22:39:31 -0400

    built out schema and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,67 @@
-export const resolvers = {
-  Query: {},
-  Mutation: {},
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (!context.user) throw new AuthenticationError("Not logged in");
+
+      const user = await User.findById(context.user._id);
+      if (!user) throw new Error("Can't find this user");
+
+      return user;
+    },
+  },
+  Mutation: {
+    login: async (parent, args) => {
+      const user = await User.findOne({ email: args.email });
+      if (!user) throw new AuthenticationError("Can't find this user");
+
+      const authenticated = await user.isCorrectPassword(args.password);
+      if (!authenticated) throw new AuthenticationError("Wrong password!");
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      if (!user) throw new AuthenticationError("Can't create user");
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (!context.user) throw new AuthenticationError("Not logged in");
+
+      const user = await User.findById(context.user._id);
+      if (!user) throw new Error("Can't find this user");
+
+      const alreadyExists =
+        user.savedBooks &&
+        user.savedBooks.some((book) => book.bookId === args.bookId);
+      if (alreadyExists) throw new Error("This book already exists");
+
+      user.savedBooks.addToSet(args);
+      await user.save();
+
+      return user.savedBooks.find((book) => book.bookId === args.bookId);
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (!context.user) throw new AuthenticationError("Not logged in");
+
+      const newUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+
+      if (!newUser) throw new Error("Unable to remove book");
+
+      return newUser;
+    },
+  },
 };
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,41 @@
-import { gql } from "apollo-server-express";
+const { gql } = require("apollo-server-express");
 
-export const typeDefs = gql`
-  type Query {}
-  type Mutation {}
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      authors: [String]!
+      description: String!
+      title: String!
+      bookId: ID!
+      image: String!
+      link: String!
+    ): Book
+    removeBook(bookId: ID!): User
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
+
+module.exports = typeDefs;
